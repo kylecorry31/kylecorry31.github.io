649dafd309e3beb0cc279fa1fb985a6c79fba742
Kyle Corry <kylecorry31@gmail.com>
2015-04-04 10:01:14 -0400

    complete material redesign

--- script.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    $('#googlePlus').mouseenter(function () {
-        $(this).attr("src", "gp.png");
-    });
-    $('#googlePlus').mouseleave(function () {
-        $(this).attr("src", "gpInv.png");
-    });
-});
