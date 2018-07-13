4c797e94c07b1dc955ba7b4f2bad1efd295d53df
Kyle Corry <kylecorry31@gmail.com>
2018-07-13 17:16:25 -0400

    Add assertions to code

--- night-mode.js
+++ night-mode.js
@@ -1,28 +1,54 @@
 window.onload = function() {
   "use strict";
-  this.nightMode = new NightMode();
-  this.nightMode.enableIfNight();
-
-  setInterval(function() {
-    this.nightMode.enableIfNight();
-  }.bind(this), 1000 * 60 * 5);
+  startNightModeDaemon();
 };
 
+/**
+ * Start the daemon which will enable night mode when it is night.
+ */
+function startNightModeDaemon() {
+  "use strict";
+  var nightMode = new NightMode();
+  nightMode.enableIfNight();
+
+  // Is this really needed?
+  setInterval(function() {
+    nightMode.enableIfNight();
+  }, 1000 * 60 * 5);
+}
+
+/**
+ * An object which can toggle the night mode of the page.
+ */
 function NightMode() {
   "use strict";
-
   this.element = document.body;
 
+  /**
+   * Enable the night mode on the page
+   */
   this.enable = function() {
+
+    assert(typeof this.element === "object");
+
     console.log("Enable night mode");
     this.element.classList.add('night');
   };
 
+  /**
+   * Disable the night mode on the page
+   */
   this.disable = function() {
+
+    assert(typeof this.element === "object");
+
     console.log("Disable night mode");
     this.element.classList.remove('night');
   };
 
+  /**
+   * Enable night mode if it is night, disable otherwise
+   */
   this.enableIfNight = function() {
     if (this.isNight()) {
       this.enable();
@@ -31,17 +57,33 @@ function NightMode() {
     }
   };
 
+  /**
+   * Determines if it is night.
+   * @return boolean - True if it is nighttime, false otherwise.
+   */
   this.isNight = function() {
     var time = new Date();
+
+    assert(typeof time === "object");
+
     // Night will be after 6 and before 7
     var hour = time.getHours();
 
     return hour >= 18 || hour <= 7;
   };
+}
 
-
-  this.isDay = function() {
-    return !this.isNight();
-  };
-
+/**
+ * An assert function. Throws an error if the assertion fails.
+ * @param  boolean - condition The condition which should be true.
+ * @param  string - message   The message to display if the assertion fails.
+ */
+function assert(condition, message) {
+  if (!condition) {
+    message = message || "Assertion failed";
+    if (typeof Error !== "undefined") {
+      throw new Error(message);
+    }
+    throw message; // Fallback
+  }
 }
