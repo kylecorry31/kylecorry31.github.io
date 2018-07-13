33deb15b687afb5a2b0ca8990352ff3af10df071
Kyle Corry <kylecorry31@gmail.com>
2018-07-13 08:49:50 -0400

    Update blog style and add reading time to articles

--- night-mode.js
+++ night-mode.js
@@ -1,45 +1,47 @@
-window.onload = function(){
-	this.nightMode = new NightMode();
-	this.nightMode.enableIfNight();
+window.onload = function() {
+  "use strict";
+  this.nightMode = new NightMode();
+  this.nightMode.enableIfNight();
 
-	setInterval(function(){
-		this.nightMode.enableIfNight();
-	}.bind(this), 1000 * 60 * 5);
+  setInterval(function() {
+    this.nightMode.enableIfNight();
+  }.bind(this), 1000 * 60 * 5);
 };
 
-function NightMode(){
+function NightMode() {
+  "use strict";
 
-	this.element = document.body;
+  this.element = document.body;
 
-	this.enable = function(){
-		console.log("Enable night mode");
-		this.element.classList.add('night');
-	};
+  this.enable = function() {
+    console.log("Enable night mode");
+    this.element.classList.add('night');
+  };
 
-	this.disable = function(){
-		console.log("Disable night mode");
-		this.element.classList.remove('night');
-	};
+  this.disable = function() {
+    console.log("Disable night mode");
+    this.element.classList.remove('night');
+  };
 
-	this.enableIfNight = function(){
-		if (nightMode.isNight()) {
-		nightMode.enable();
-		} else {
-			nightMode.disable();
-		}
-	};
+  this.enableIfNight = function() {
+    if (this.isNight()) {
+      this.enable();
+    } else {
+      this.disable();
+    }
+  };
 
-	this.isNight = function(){
-		var time = new Date();
-		// Night will be after 6 and before 7
-		var hour = time.getHours();
+  this.isNight = function() {
+    var time = new Date();
+    // Night will be after 6 and before 7
+    var hour = time.getHours();
 
-		return hour >= 18 || hour <= 7;
-	};
+    return hour >= 18 || hour <= 7;
+  };
 
 
-	this.isDay = function(){
-		return !this.isNight();
-	};
+  this.isDay = function() {
+    return !this.isNight();
+  };
 
 }
